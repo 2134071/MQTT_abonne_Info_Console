f7a957abcbe0e844c365b4300616d89f06022340
Alexis Lavertu <2134071@cegepba.qc.ca>
2023-09-06 13:23:31 -0400

    ajout des donnée demander dans le temps

--- MQTTAbonneInfo/main.py
+++ MQTTAbonneInfo/main.py
@@ -1,14 +1,14 @@
-# This is a sample Python script.
 import time
 
 import paho.mqtt.client as mqtt
 
 # Fonction callback pour la connexion au serveur MQTT
 def on_connect(client, userdata, flags, rc):
+    print(flags)
     if rc == 0:
         print("Connecté au serveur MQTT")
-        client.subscribe(MQTT_TOPIC)  # S'abonne au sujet "topic/version"
-        print("subribe"+MQTT_TOPIC)
+        client.subscribe(MQTT_TOPIC,qos=1)  # S'abonne au sujet "topic/version"
+        print("subscribe "+MQTT_TOPIC)
     else:
         print(f"Échec de la connexion au serveur MQTT avec le code d'erreur: {rc}")
 
@@ -19,8 +19,10 @@ def on_message(client, userdata, msg):
 # Créez un client MQTT
 MQTT_BROKER = "10.4.1.42"
 MQTT_TOPIC = "jacob/temperature/sensor"
-client = mqtt.Client()
+client = mqtt.Client(client_id="Alexis", clean_session=False)
 
+#client.reinitialise()
+#max_queued_messages_set(self, queue_size)
 # Définir les fonctions de callback
 client.on_connect = on_connect
 client.on_message = on_message
@@ -30,6 +32,8 @@ client.connect(MQTT_BROKER, 1883, 60)
 
 # Boucle principale pour maintenir la connexion et traiter les messages
 time.sleep(2)
-client.loop_forever()
-
+try:
+    client.loop_forever()
+except KeyboardInterrupt:
+    client.disconnect()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
